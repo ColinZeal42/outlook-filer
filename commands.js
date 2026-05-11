a441354b8b7cfc6dbcd68f6036fb878cce26b106
ColinZeal42 <chris.montville@gmail.com>
2026-05-10 22:14:14 -0600

    Diagnostic: allow-only handler to test if softblock is the problem

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 "use strict";
 
 Office.actions.associate("onMessageSend", function(event) {
-  event.completed({ allowEvent: false, errorMessage: "v9: handler ran." });
+  event.completed({ allowEvent: true });
 });
